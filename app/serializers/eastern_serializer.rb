8e8445c62deaf6d4a9bb62dbbdc9a77025ebefce
github username <github email address>
2018-01-23 15:10:28 -0500

    Removed :horoscope from eastern serializer

--- app/serializers/eastern_serializer.rb
+++ app/serializers/eastern_serializer.rb
@@ -1,5 +1,5 @@
 class EasternSerializer < ActiveModel::Serializer
-  attributes :id, :sign, :description, :lucky_number, :lucky_color, :img_url, :eastern_partners, :horoscope
+  attributes :id, :sign, :description, :lucky_number, :lucky_color, :img_url, :eastern_partners
   # has_many :compatibilities
   # has_many :eastern_partners, through: :compatibilities, foreign_key: :eastern_partner_id, source: :eastern
   # has_many :users
