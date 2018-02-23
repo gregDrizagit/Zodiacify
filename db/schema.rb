5620e09ae3d77ec47dc426e49e7196831c72982c
Elli Shim <ellishim@gmail.com>
2018-02-24 08:58:24 +0900

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180123190548) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "compatibilities", force: :cascade do |t|
     t.integer "eastern_id"
     t.integer "eastern_partner_id"
