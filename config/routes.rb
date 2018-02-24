78ec92fcdb31aff967d0a6762f35bddd2deb317e
Elli Shim <ellishim@gmail.com>
2018-02-24 09:22:52 +0900

    default page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, :easterns, :westerns, :compatibilities
   post "/auth", to: "auth#create"
   get "/current_user", to: "auth#show"
+  root "application#index"
 end
